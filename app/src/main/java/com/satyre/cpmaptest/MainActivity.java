cd51bffa335f8ca21cada1be5f447e8aee10b996
Satyre <robin.maitre@gmail.com>
2017-06-28 18:15:09 +0200

    test for étape 3

--- app/src/main/java/com/satyre/cpmaptest/MainActivity.java
+++ app/src/main/java/com/satyre/cpmaptest/MainActivity.java
@@ -3,6 +3,9 @@ package com.satyre.cpmaptest;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.PointF;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,8 +18,11 @@ import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import com.google.gson.JsonElement;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -26,7 +32,13 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.services.android.ui.geocoder.GeocoderAutoCompleteView;
 import com.mapbox.services.api.geocoding.v5.GeocodingCriteria;
 import com.mapbox.services.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.services.commons.geojson.Feature;
 import com.mapbox.services.commons.models.Position;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     private final static String MAPBOX_KEY = "pk.eyJ1Ijoic2F0eXJlIiwiYSI6ImNqNGVpbW96aDE1N2UzM2x2aDRra28zeDUifQ.hZ5xMiJnOkqU2QR-muCiBQ";
     private MapView mapView;
     private MapboxMap map;
+    private MarkerView lastMarkerAdd;
+    GeocoderAutoCompleteView autocomplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
                                     .newCameraPosition(position), 4000); // move camera on position in desire time (ms)
                         }
                         // Set up autocomplete widget
-                        GeocoderAutoCompleteView autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.autoCompleteView);
+                        autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.autoCompleteView);
                         autocomplete.setAccessToken(Mapbox.getAccessToken());
                         autocomplete.setType(GeocodingCriteria.TYPE_ADDRESS);
                         autocomplete.setCountry("FR");
@@ -144,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                 });
-
 
             }
         });
@@ -205,18 +218,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateMap(double latitude, double longitude, String address) {
-        // Build marker
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(latitude, longitude))
-                .title(address));
+        final LatLng pinLocation = new LatLng(latitude, longitude);
 
-        Log.d(LOG_TAG, "address : " + address);
-        // Animate camera to geocoder result location
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(latitude, longitude))
-                .zoom(15)
-                .build();
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
+        if(lastMarkerAdd == null) {
+            // Build marker
+            map.addMarker(new MarkerOptions()
+                    .position(pinLocation)
+                    .title("Pick up :")
+                    .snippet(address));
+
+            Log.d(LOG_TAG, "address : " + address);
+            // Animate camera to geocoder result location
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(pinLocation)
+                    .zoom(15)
+                    .build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
+
+            map.setOnCameraChangeListener(new MapboxMap.OnCameraChangeListener() {
+                @Override
+                public void onCameraChange(CameraPosition position) {
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(pinLocation)
+                            .build();
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 0, null);
+                }
+            });
+        }
+        map.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng point) {
+                if (lastMarkerAdd != null)
+                    lastMarkerAdd.setPosition(point);
+                else
+                    lastMarkerAdd = map.addMarker(new MarkerViewOptions()
+                            .position(point)
+                            .title("Arrival :"));
+
+
+
+
+
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(point.getLatitude(), point.getLongitude(), 1);
+                    if(null!=listAddresses&&listAddresses.size()>0){
+                        String addressLine = listAddresses.get(0).getAddressLine(0);
+                        String zipcode = listAddresses.get(0).getPostalCode();
+                        String city = listAddresses.get(0).getLocality();
+                        autocomplete.setText(addressLine + " " + zipcode + " " + city);
+                        lastMarkerAdd.setSnippet(addressLine + " " + zipcode + " " + city);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
 
